ad2729061f37728558f5d163a886e47bfd70a544
Khoi <kmphan@ucsd.edu>
2020-03-09 10:48:13 +0000

    Finished Fib number

--- index.js
+++ index.js
@@ -190,4 +190,17 @@ const numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
 //   return num % 7 !== 0;
 // }));
 
+//Extra: Fibonacci Number. Function returns array of fib numbers. Input is greater than 2
+// function fib(num) {
+//   if(num === 1)
+//     return [1];
+//   else if (num === 2)
+//     return [1,1];
 
+//   let array = [1, 1];
+//   for(let i=2; i<num;i++) {
+//     array.push(array[i-1] + array[i-2]);
+//   }
+//   return array;
+// }
+// console.log("Fib numbers ", fib(10));
